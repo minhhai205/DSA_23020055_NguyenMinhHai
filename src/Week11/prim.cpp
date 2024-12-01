faa6e28234e32a2c3f1f1bdc2530458ceea46f89
minhhai205 <158561042+minhhai205@users.noreply.github.com>
2024-12-01 23:14:30 +0700

    Update prim.cpp

--- src/Week11/prim.cpp
+++ src/Week11/prim.cpp
@@ -20,6 +20,16 @@ using namespace std;
  * 4. Thêm cạnh u - Parent(u) vào tập cạnh thuộc cây khung nhỏ nhất.
  */
 
+/**
+ * Độ phức tạp của thuật toán:
+ * 
+ * Duyệt qua tất cả các đỉnh và cạnh:
+ *   - Duyệt qua các đỉnh: O(V).
+ *   - Mỗi đỉnh duyệt qua danh sách kề của nó: O(E).
+ *   - Các thao tác cập nhật priority queue: O(log V) với mỗi cạnh.
+ * Do đó, tổng độ phức tạp là: O((V + E) log V)
+ */
+
 struct Edge {
     int u, v, w;
 };
@@ -29,18 +39,23 @@ class Graph{
 private:
     int V;
     int E;
-    vector<Edge> edgeList;
+    vector<vector<pair<int, int>>> adj;
 public:
-    Graph(int v){ V = v; E = 0; }
-    
+    Graph(int v){ 
+        adj.resize(v + 1);
+        V = v; 
+        E = 0; 
+    }
+
     int getV() { return V; }
 
     int getE() { return E; }
 
-    vector<Edge> getEdges(){ return edgeList; }
+    vector<vector<pair<int, int>>> getAdj(){ return adj; }
 
     void addEdge(int u, int v, int w){
-        edgeList.push_back({u, v, w});
+        adj[u].push_back({v, w});
+        adj[v].push_back({u, w});
         ++E;
     }
 };
@@ -50,13 +65,50 @@ private:
     vector<Edge> edgeList;
     int totalWeight;
 
-    void prim(int u){
+    void prim(int s, Graph graph){
+        vector<vector<pair<int, int>>> adj = graph.getAdj();
+        
+        int dis[graph.getV() + 1];  // mảng lưu khoảng cách của toàn bộ đỉnh
 
+        for (int i = 1; i <= graph.getV(); i++) dis[i] = INT_MAX;
+
+        int parent[graph.getV() + 1];
+        
+        // các phần tử lưu trong priority queue sẽ có dạng pair<dis[u],u>
+        priority_queue<pair<int, int>, vector<pair<int,int>>, greater<pair<int,int>>> q;
+
+        dis[s] = 0;
+        q.push({0, s});
+
+        while(!q.empty()){
+            auto top = q.top(); q.pop();
+
+            int currDis = top.first, u = top.second;
+
+            if(currDis != dis[u]) continue;
+
+            totalWeight += dis[u]; 
+            dis[u] = INT_MIN;
+
+            if(u != s) {
+                edgeList.push_back({parent[u], u, currDis});
+            }
+
+            for(auto it : adj[u]){
+                int v = it.first, w = it.second;
+                if(w < dis[v]){
+                    dis[v] = w;
+                    parent[v] = u;
+                    q.push({w, v});
+                }
+            }
+        }
     }
 
 public:
     Mst(Graph graph){ 
         totalWeight = 0; 
+        prim(1, graph);
     }
 
     vector<Edge> getEdges(){ return edgeList; }
@@ -67,20 +119,26 @@ public:
 
 
 int main() {
-    Graph graph(4);
-
-    graph.addEdge(1, 2, 10);
-    graph.addEdge(2, 3, 15);
-    graph.addEdge(1, 3, 5);
-    graph.addEdge(3, 4, 10);
+    Graph graph(7);  
+    graph.addEdge(1, 2, 7);
+    graph.addEdge(1, 3, 6);
+    graph.addEdge(1, 4, 5);
+    graph.addEdge(2, 3, 8);
+    graph.addEdge(2, 4, 9);
+    graph.addEdge(2, 5, 7);
+    graph.addEdge(3, 5, 9);
+    graph.addEdge(4, 5, 15);
+    graph.addEdge(4, 6, 8);
+    graph.addEdge(5, 6, 5);
+    graph.addEdge(5, 7, 10);
+    graph.addEdge(6, 7, 2);
 
     Mst mst(graph);
 
-    cout << "Total weight of MST: " << mst.getWeight() << endl;
+    cout << "Tổng trọng số của cây khung nhỏ nhất: " << mst.getWeight() << endl;
 
-    cout << "Edges in MST:" << endl;
-    for (auto edge : mst.getEdges()) {
-        cout << "(" << edge.u << ", " << edge.v << ") - " << edge.w << endl;
+    for (const auto& edge : mst.getEdges()) {
+        cout << "Cạnh: " << edge.u << " - " << edge.v << " với trọng số: " << edge.w << endl;
     }
 
 }
